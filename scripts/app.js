59de88897d56ef69bc7417ba78fa0a5837e23a41
addison912 <addisondrewmoore@gmail.com>
2018-11-18 21:18:12 -0800

    initial commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,35 @@
-// define globals
-var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
-
+let icon = "./images/earthquake24.png";
+let weekly_quakes_endpoint =
+  "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+let features = [];
 $(document).ready(function() {
-  console.log("Let's get coding!");
-  // CODE IN HERE!
-
+  $.ajax({
+    url: weekly_quakes_endpoint,
+    method: "GET",
+    success: function(response) {
+      console.log(response);
+      response.features.forEach(function(feature) {
+        features.push(feature);
+        $("#info").append(`<p>${feature.properties.title}</p>`);
+      });
+      function initMap() {
+        let map = new google.maps.Map(document.getElementById("map"), {
+          center: { lat: 37.8044, lng: -122.2711 },
+          zoom: 5
+        });
+        features.forEach(function(feature) {
+          let location = {
+            lat: feature.geometry.coordinates[1],
+            lng: feature.geometry.coordinates[0]
+          };
+          let marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            icon: icon
+          });
+        });
+      }
+      initMap();
+    }
+  });
 });
